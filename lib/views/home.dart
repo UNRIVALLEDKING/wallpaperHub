96d3d6a2e207f7aa079b5abc1d69d9f61c823ec7
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-06-04 22:07:46 +0530

    Wallpaper Hub

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -12,6 +12,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: brandName(),
@@ -21,20 +22,41 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             Container(
+              decoration: BoxDecoration(
+                  color: Color(0x0FFF5F8FD),
+                  borderRadius: BorderRadius.circular(30)),
+              // color: Colors.blue,
+              padding: EdgeInsets.symmetric(horizontal: 14),
+              margin: EdgeInsets.symmetric(horizontal: 24),
               child: Row(
                 children: <Widget>[
                   Expanded(
                     child: TextField(
-                      decoration: InputDecoration(hintText: "Search"),
+                      decoration: InputDecoration(
+                          hintText: "Search Wallpaper",
+                          border: InputBorder.none),
                     ),
                   ),
                   Icon(Icons.search)
                 ],
               ),
             ),
+
+            SizedBox(height: 16,),
+
           ],
         ),
       ),
     );
+  }
+}
+
+
+class CategoriesList extends StatelessWidget {
+  const CategoriesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
