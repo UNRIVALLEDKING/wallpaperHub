b65165bf965265ec4f8e26f7fbcef488c7c248f9
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-06-05 22:54:01 +0530

    model

--- lib/model/categories_model.dart
+++ lib/model/categories_model.dart
@@ -1,4 +1,15 @@
+// This code has some warnings
+
+// class CategoriesModel {
+//   String categoryName;
+//   String imgUrl;
+// }
+
+// Warnings solved
+
 class CategoriesModel {
-  String categoryName;
-  String imgUrl;
+  late String categoryName;
+  late String imgUrl;
+
+  CategoriesModel({required this.categoryName, required this.imgUrl});
 }
