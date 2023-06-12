ec71291e45a1c2e807846461ed064ea5c0884cf2
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-06-12 20:32:25 +0530

    widgets

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaperhub/model/wallpaper_model.dart';
 
 // Widget BrandName() {
 //   return Row(
@@ -33,5 +34,32 @@ Widget brandName() {
       ),
       Text("Hub", style: TextStyle(color: Colors.blue)),
     ],
+  );
+}
+
+Widget wallpapersList(List<WallpaperModel> wallpapers, context) {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    child: GridView.count(
+      physics: ClampingScrollPhysics(),
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      childAspectRatio: 0.6,
+      mainAxisSpacing: 6.0,
+      crossAxisSpacing: 6.0,
+      children: wallpapers.map((wallpaper) {
+        return GridTile(
+          child: Container(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.network(
+                wallpaper.src?.portrait ?? "",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        );
+      }).toList(),
+    ),
   );
 }
